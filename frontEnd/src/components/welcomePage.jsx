991723bb807f12d9ee550afa96fbfa80004ba07f
Modekai <muzyar.rad@gmail.com>
2020-05-20 09:45:22 -0700

    welcome page messge changed and adjusted

--- frontEnd/src/components/welcomePage.jsx
+++ frontEnd/src/components/welcomePage.jsx
@@ -4,7 +4,16 @@ import "../css/welcomePage.css";
 const WelcomePage = props => {
   const user = props.user;
   if (user != null) window.location = "/home";
-  return <div className="text">Welcome to MazyBook</div>;
+  return (
+    <div className="welcome">
+      Welcome to Mazybook <br />
+      <br /> This social media application requires a username and password to
+      experience properly. Please use the following demo login or feel free to
+      register to create your own credentials. <br />
+      <br /> Username: demo <br /> Password: test <br />
+      <br /> Thank you for visiting my application!
+    </div>
+  );
 };
 
 export default WelcomePage;
