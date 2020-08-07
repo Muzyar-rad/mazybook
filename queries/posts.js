38870a185bbc6ddaf8eccaa7612f1bf911c45402
Modekai <muzyar.rad@gmail.com>
2020-08-06 18:42:45 -0700

    changed the codebase

--- queries/posts.js
+++ queries/posts.js
@@ -38,7 +38,7 @@ router.get("/", async (req, res) => {
   );
   res.status(200).send(rows);
 });
-
+('Select * from "Post"');
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   const { rows } = await pool.query('SELECT * FROM "Post" WHERE postid = $1', [
